2ddc90611c407220002ab0498abb34e9170a7792
Kent Sumbilon <kntsmbln1@gmail.com>
2023-04-07 20:14:03 +0800

    added menu-overlay

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
 const menuOverlay = document.querySelector('.menu-overlay');
 
+// Add event listener to hamburger button to toggle menu
 hamburger.addEventListener('click', function() {
   menuOverlay.classList.toggle('open');
+});
+
+// Add event listener to close button to hide menu
+const closeButton = document.querySelector('.close-button');
+closeButton.addEventListener('click', function() {
+  menuOverlay.classList.remove('open');
 });
