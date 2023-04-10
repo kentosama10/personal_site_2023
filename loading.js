3b4f2e1ba8c353b95a85de2f358590f110409f63
Kent Sumbilon <kntsmbln1@gmail.com>
2023-04-11 00:01:11 +0800

    added loading screen

---
+++ loading.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", function(){
+  const loader = document.getElementById("loader-container");
+  const container = document.getElementById("home-page-container");
+  const loadingBar = document.getElementById("loading-bar");
+  let width = 0;
+  const interval = setInterval(function(){
+    if (width >= 100) {
+      clearInterval(interval);
+      setTimeout(function(){
+        loader.style.opacity = "0";
+        setTimeout(function(){
+          loader.style.display = "none";
+          container.style.opacity = "1";
+        }, 1000);
+      }, 500);
+    } else {
+      width += 1;
+      loadingBar.style.width = width + "%";
+    }
+  }, 20);
+});
