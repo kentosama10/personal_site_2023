fd57807f2f54a9e86aff6f26456833a7df1ce10d
Kent Sumbilon <kntsmbln1@gmail.com>
2023-04-11 11:27:49 +0800

    updated loading screen

--- loading.js
+++ loading.js
@@ -1,27 +1,23 @@
+document.body.style.overflow = "hidden";
 window.addEventListener("load", function(){
   const loader = document.getElementById("loader-container");
   const container = document.getElementById("home-page-container");
   const loadingBar = document.getElementById("loading-bar");
   let width = 0;
-  const loaderFadeOutDuration = 1000;
-  const containerFadeInDuration = 1000;
-
-  function animateLoadingBar(timestamp) {
+  const interval = setInterval(function(){
     if (width >= 100) {
+      clearInterval(interval);
       setTimeout(function(){
-        loader.style.transition = `opacity ${loaderFadeOutDuration}ms`;
         loader.style.opacity = "0";
-        container.style.transition = `opacity ${containerFadeInDuration}ms`;
-        container.style.opacity = "1";
-        requestAnimationFrame(function(){
+        setTimeout(function(){
           loader.style.display = "none";
-        });
+          container.style.opacity = "1";
+          document.body.style.overflow = "auto";
+        }, 0);
       }, 500);
     } else {
-      width += 1;
+      width += 5;
       loadingBar.style.width = width + "%";
-      requestAnimationFrame(animateLoadingBar);
     }
-  }
-  requestAnimationFrame(animateLoadingBar);
+  }, 10);
 });
